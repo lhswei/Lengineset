1c3c620364099d397b1f40d28ee6d0b6522bdef8
lhswei <609741@163.com>
2018-01-08 11:02:39 +0800

    md

--- project/test/node/syntax/main.cpp
+++ project/test/node/syntax/main.cpp
@@ -6,6 +6,11 @@
 class Base;
 void tprint(Base* p);
 
+struct no_st
+{
+
+};
+
 class Base
 {
 public:
@@ -50,31 +55,40 @@ char* get_str()
     return p;
 }
 
+void new_big()
+{
+    char big[1024*1024*10];
+    memset(big, 0, sizeof(big));
+}
+
 int main(int argc, char *argv[])
 {
 
-	char str1[] = "hello";
-	char str2[] = {'h', 'e', 'l', 'l', 'o'};
-	char str3[] = {'h', 'e', 'l', 'l', 'o', '\0'};
+	// char str1[] = "hello";
+	// char str2[] = {'h', 'e', 'l', 'l', 'o'};
+	// char str3[] = {'h', 'e', 'l', 'l', 'o', '\0'};
 
-	printf("%d, %d\n", sizeof(str1), strlen(str1));
-	printf("%d, %d\n", sizeof(str2), strlen(str2));//linux 32位 c++ 4.1.0 是（5， 10） linux 64 位 5.2 是 （5， 5）
-	printf("%d, %d\n", sizeof(str3), strlen(str3));
+	// printf("%d, %d\n", sizeof(str1), strlen(str1));
+	// printf("%d, %d\n", sizeof(str2), strlen(str2));//linux 32位 c++ 4.1.0 是（5， 10） linux 64 位 5.2 是 （5， 5）
+	// printf("%d, %d\n", sizeof(str3), strlen(str3));
 
-    char *p = get_str();
-    printf("%s\n", p);
+    // char *p = get_str();
+    // printf("%s\n", p);
 
-    Base* pt  = new Driver();
-    delete pt;
-    printf("\n");
+    // Base* pt  = new Driver();
+    // delete pt;
+    // printf("\n");
 
-    int i = 0;
-    for(i = 0; i < 2 ; i++)
-    {
-        fork();
-        printf("A\n");
-    }
+    // int i = 0;
+    // for(i = 0; i < 2 ; i++)
+    // {
+    //     fork();
+    //     printf("A\n");
+    // }
 
+    // printf("%d\n", sizeof(no_st));
+    
+    new_big();
 	return 0;
 
 }
