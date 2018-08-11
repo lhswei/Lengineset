c041695fcddf6f68876364b5344b2afe25751c98
lhswei <609741@163.com>
2018-08-11 14:01:32 +0800

    md

--- include/MyDelegate.h
+++ include/MyDelegate.h
@@ -4,6 +4,46 @@
 #include <list>
 #include <vector>
 
+//人任意参数，任意返回值的代理 
+
+// sample code
+/*
+using namespace Delegate;
+ 
+void NormalFunc(int a)
+{
+	printf("这里是普通函数 ：%d\n", a);
+}
+ 
+class A
+{
+public:
+	static void StaticFunc(int a)
+	{
+		printf("这里是成员静态函数 ： %d\n", a);
+	}
+	void MemberFunc(int a)
+	{
+		printf("这里是成员非静态函数 ： %d\n", a);
+	}
+};
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//首先创建了一个返回值为 void ,参数为int 的一个委托。
+	CMultiDelegate<void, int> e;
+ 
+	//将三个函数注册到该委托中
+	e += newDelegate(NormalFunc);
+	e += newDelegate(A::StaticFunc);
+	e += newDelegate(&A(), &A::MemberFunc);
+ 
+	//调用
+	e(1);
+ 
+	return 0;
+}
+*/
+
 namespace Delegate
 {
 
